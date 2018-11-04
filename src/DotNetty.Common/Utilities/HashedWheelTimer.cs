c3c8ac660d8cfb0fb932020f794f0c76aeb7b54b
mikelchai <chaib@outlook.com>
2018-11-03 20:32:51 -0700

    Fix the exception message in ExpireTimeouts (#440)

--- src/DotNetty.Common/Utilities/HashedWheelTimer.cs
+++ src/DotNetty.Common/Utilities/HashedWheelTimer.cs
@@ -619,7 +619,7 @@ namespace DotNetty.Common.Utilities
                             // The timeout was placed into a wrong slot. This should never happen.
                             throw new InvalidOperationException(
                                 string.Format(
-                                    "timeout.deadline (%d) > deadline (%d)",
+                                    "timeout.deadline {0} > deadline {1}",
                                     timeout.Deadline,
                                     deadline));
                         }
